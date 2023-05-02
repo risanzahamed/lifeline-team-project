c24b38033122cb1ddabffdf3a9a697e6c112943f
Risan Ahamed <risanzahamed76@gmail.com>
2023-05-02 20:33:18 +0600

    first commit

--- Components/JoinVulenteers/JoinVulenteers.tsx
+++ Components/JoinVulenteers/JoinVulenteers.tsx
@@ -8,7 +8,7 @@ const JoinVulenteers = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left" data-aos="fade-down-right" data-aos-duration="3000">
                         <h1 className="text-[24px] lg:text-[36px] font-bold lg:text-3xl">Join Our Volunteer</h1>
-                        <p className="py-2 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consequuntur nesciunt modi, obcaecati provident, non doloribus earum odit v is tenetur autem perferendis voluptates aspernatur ipsa corrupti unde placeat odio asperiores velit exercitationem sint at ex harum neque ipsum! Eos soluta nobis saepe officia mollitia dolores cupiditate, accusamus eum ab pariatur dicta eaque excepturi at vitae maxime consequuntur dolor eius vel voluptatem ipsam culpa quo?</p>
+                        <p className="py-2 text-justify">Join Our VolunteerJoin our volunteer team and make a positive impact in your community. Together, we can create meaningful change and help those in need. Join us today and be a part of something bigger!</p>
                         <label htmlFor="joinVolunterers" className="btn btn-primary mt-2" >Join Now</label >
 
                     </div>
